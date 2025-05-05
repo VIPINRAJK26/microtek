a20fa062a333f253d1a72bef8d1826bbc224e239
VIPINRAJK26 <vipinrajk026@gmail.com>
2025-05-05 17:43:35 +0530

    updated axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://server.warriorind.in/api/",
+  baseURL: "https://server.warriorind.in/api/",
   headers: {
     "Content-Type": "application/json",
   },
